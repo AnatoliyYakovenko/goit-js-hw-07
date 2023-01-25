545d8d9a5d61d596512f7513e25e4acb5513b668
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-01-25 04:46:23 +0200

    complete task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+  />`;
+  })
+  .join("");
+
+galleryEl.innerHTML = markup;
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
